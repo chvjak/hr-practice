e61dac16a7cc66e84b028d7da3437f93ff3363aa
doa <doa@simcorp.com>
2018-04-13 16:19:17 +0300

    dijkstra, graph visualisation

--- jack_goes_to_rapture.py
+++ jack_goes_to_rapture.py
@@ -13,53 +13,18 @@ import heapq
 from collections import defaultdict
 
 
-class Element:
-    def __init__(self, v):
-        self.v = v
-        self.deleted = False
-
-    def __lt__(self, other):
-        return True
-
-    def __repr__(self):
-        return "[{}, {}]".format(self.v, self.deleted)
-
-
 class PQ():
     def __init__(self):
         self.data = []
-        self.v2el = {}
-        self.count = 0
 
     def enque(self, k, v):
-        e = Element(v)
-        heapq.heappush(self.data, (k, e))
-        self.v2el[v] = e
-        self.count += 1
+        heapq.heappush(self.data, (k, v))
 
     def deque(self):
-        while len(self.data):
-            k, e = heapq.heappop(self.data)
-            if not e.deleted:
-                v = e.v
-                del self.v2el[v]
-                break
-        else:
-            return None, None
-
-        self.count -= 1
-        return k, v
-
-    def update_key(self, k, v):
-        if v not in self.v2el.keys():
-            return
-
-        self.v2el[v].deleted = True
-        self.enque(k, v)
-        self.count -= 1
+        return heapq.heappop(self.data)
 
     def __len__(self):
-        return self.count
+        return len(self.data)
 
 
 def dijkstra(adj, weights, s, t, N):
@@ -67,10 +32,7 @@ def dijkstra(adj, weights, s, t, N):
 
     pq = PQ()
 
-    for v in range(N):
-        pq.enque(sys.maxsize, v)
-
-    pq.update_key(s, 0)
+    pq.enque(s, 0)
     dist_to[s] = 0
 
     while len(pq):
@@ -79,7 +41,7 @@ def dijkstra(adj, weights, s, t, N):
             W = max(0, weights[(v, vn)] - dist_to[v])
             new_dist = dist_to[v] + W
             if dist_to[vn] > new_dist:
-                pq.update_key(new_dist, vn)
+                pq.enque(new_dist, vn)
                 dist_to[vn] = new_dist
 
     return dist_to[t]
@@ -102,12 +64,8 @@ if __name__ == '__main__':
         adj[v_from].add(v_to)
         adj[v_to].add(v_from)
 
-        if (v_from, v_to) in weights.keys():
-            print('DUPLICATE')
-            break
-        else:
-            weights[(v_from, v_to)] = weight
-            weights[(v_to, v_from)] = weight
+        weights[(v_from, v_to)] = weight
+        weights[(v_to, v_from)] = weight
 
     res = dijkstra(adj, weights, 0, g_nodes - 1, g_nodes)
     if res == sys.maxsize:
